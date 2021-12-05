7ed799c9482b31517bb843ea181aa052744a9f4a
Maleriya <50017431+Maleriya@users.noreply.github.com>
2021-12-06 00:09:11 +0300

    Интеграция и настройка 2D-ассетов

---
+++ Assets/Scripts/SpriteAnimationsConfig.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Track
+{
+    idle,
+    run,
+    jump
+}
+
+[CreateAssetMenu(fileName = "SpriteAnimationsCnfg", menuName = "Configs/SpriteAnimationsCnfg", order = 1)]
+public class SpriteAnimationsConfig : ScriptableObject
+{
+    [Serializable]
+    public class SpritesSequence
+    {
+        public Track Track;
+        public List<Sprite> Sprites = new List<Sprite>();
+    }
+    public List<SpritesSequence> Sequences = new List<SpritesSequence>();
+}
